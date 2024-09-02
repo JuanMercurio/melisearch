a5da639ad3d5661e40c6de551dec9779d72ef9c9
Juan Mercurio <juanmercuriobadii@gmail.com>
2024-09-02 09:28:32 -0300

    Add temporary repository

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -33,43 +31,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	type ejemplo struct {
-		Saturno string `bson:"saturno"`
-		Jupiter string `bson:"jupiterrrrrr"`
-	}
-
-	e := ejemplo{
-		"planeta con anillos",
-		"planeta grade",
-	}
-
-	// nos conectamos a una base y una collection
-	collection := client.Database("testing").Collection("colltest")
-	result, err := collection.InsertOne(ctx, e)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// nos busca la "tabla" entera
-	cur, err := collection.Find(ctx, e)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cur.Close(ctx)
-
-	// iteramos sobre las "filas" / documento
-	for cur.Next(ctx) {
-		var result bson.D
-		err := cur.Decode(&result)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(result)
-	}
-	if err := cur.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("id:")
-	fmt.Println(result.InsertedID)
+	_ = NewRepository(client.Database("testing"))
+	return
 }
---
+++ repository.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Repository struct {
+	db *mongo.Database
+}
+
+func NewRepository(db *mongo.Database) *Repository {
+	return &Repository{
+		db: db,
+	}
+}
+
+func (r *Repository) NewSynonyms(ctx context.Context, word string, newSynonyms []string) error {
+
+	col := r.db.Collection("synonyms")
+
+	filter := bson.M{"word": word}
+	update := bson.M{
+		"$addToSet": bson.M{"synonyms": bson.M{"$each": newSynonyms}},
+	}
+	options := options.Update().SetUpsert(true)
+
+	_, err := col.UpdateMany(ctx, filter, update, options)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Repository) List(ctx context.Context, word string) {
+	col := r.db.Collection("synonyms")
+
+	filter := bson.M{"word": word}
+
+	cur, err := col.Find(ctx, filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+
+	var synonyms struct {
+		Synonyms []string `json:"synonyms"`
+	}
+
+	// esto solofunciona si tiene 1 doc de esa palabra
+	for cur.Next(ctx) {
+		err := cur.Decode(&synonyms)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println(synonyms.Synonyms)
+
+}
